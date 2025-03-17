54875556f25130251f0da93c3663f8c30a2a2482
aitorortega03 <aitorortega21@gmail.com>
2025-03-17 20:56:51 +0100

    Separate useEffect in two

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,31 @@ const RANDOM_FACT_URL = 'https://catfact.ninja/fact?max_length=300'
 // const CAT_IMAGE_URL = `https://cataas.com/cat/says/${}`
 
 export default function App () {
-  const [fact, setFact] = useState('hello')
-  const [imageUrl, setImageUrl] = useState('')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
+  // recuperar fact
   useEffect(() => {
     fetch(RANDOM_FACT_URL)
       .then(response => response.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-        const firstWord = fact.split(' ')[0] // para las 3 primeras palabras sería fact.split(' ').slice(0, 3).join(' ') o fact.split(' ', 3)
-
-        fetch(`https://cataas.com/cat/says/${firstWord}`)
-          .then(response => response.json())
-          .then(data => {
-            const { url } = data
-            setImageUrl(url)
-          })
       })
   }, [])
+
+  // recuperar img teniendo el fact
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ')[0] // para las 3 primeras palabras sería fact.split(' ').slice(0, 3).join(' ') o fact.split(' ', 3)
+
+    fetch(`https://cataas.com/cat/says/${firstWord}`)
+      .then(response => response.json())
+      .then(data => {
+        const { url } = data
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
